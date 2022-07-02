a9c55f7645dda3664de110224714a0426f09b565
Saba-Var <vartasashvili94@gmail.com>
2022-07-02 21:35:38 +0400

    styles: make band info page scrollable

--- src/pages/About/components/BandInfo.tsx
+++ src/pages/About/components/BandInfo.tsx
@@ -3,14 +3,14 @@ import { BandImage } from 'pages/About/components'
 
 const BandInfo: React.FC<BandInfoProps> = (props) => {
   return (
-    <div
-      className={`h-full relative w-[110%] pr-[5%] overflow-y-hidden !overflow-x-hidden`}
-    >
+    <div className='h-full  overflow-y-auto w-[105%] pr-[5%]'>
       <BandImage isLoading={props.isLoading} image={props.image} />
-      <textarea
+      <div
         defaultValue={props.about}
-        className='w-full break-words pt-[20%] pr-[5%] bg-transparent outline-none resize-none !h-full font-BPG-Arial text-base tracking-[0.2px]'
-      />
+        className='overflow-hidden whitespace-pre pt-8 bg-transparent resize-none outline-none w-full h-max'
+      >
+        {props.about}
+      </div>
     </div>
   )
 }
