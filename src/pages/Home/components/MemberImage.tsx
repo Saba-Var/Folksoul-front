4ce403025eb46622e70b0f7979ba55f61f810ea5
Saba-Var <vartasashvili94@gmail.com>
2022-07-05 19:19:07 +0400

    chore: destruct from member image props

--- src/pages/Home/components/MemberImage.tsx
+++ src/pages/Home/components/MemberImage.tsx
@@ -1,14 +1,16 @@
 import { MemberImageProps } from 'pages/Home/components/types'
 
 const MemberImage: React.FC<MemberImageProps> = (props) => {
+  const { image, color, imageArray, index, setInfoImage } = props
+
   const clickHandler = () => {
     props.setPause(true)
     props.setInfoText(props.biography)
-    if (props.image) props.setInfoImage(props.image)
-    else
-      props.setInfoImage(props.imageArray[props.index] || props.imageArray[0])
 
-    props.setColor(props.color || '#333333')
+    if (image) setInfoImage(image)
+    else setInfoImage(imageArray[index] || imageArray[0])
+
+    props.setColor(color || '#333333')
   }
 
   return (
@@ -23,34 +25,32 @@ const MemberImage: React.FC<MemberImageProps> = (props) => {
       <div className=' border-[2px] border-yellow rounded-full '>
         <div
           style={{
-            backgroundColor: props.color || 'black',
+            backgroundColor: color || 'black',
           }}
           className='w-[75px] h-[75px] flex justify-center rounded-full overflow-hidden'
         >
-          {!props.image && (
+          {!image && (
             <img
               className='h-full w-full'
               src={
-                props.imageArray[props.index] ||
-                props.imageArray[
-                  Math.abs(props.imageArray.length - props.index)
-                ]
+                imageArray[index] ||
+                imageArray[Math.abs(imageArray.length - index)]
               }
               alt='member'
             />
           )}
 
-          {props.image && (
+          {image && (
             <img
               className='h-24 w-24'
-              src={`http://localhost:5000/${props.image}`}
+              src={`http://localhost:5000/${image}`}
               alt='member'
             />
           )}
         </div>
         <div
           style={{
-            borderColor: props.color,
+            borderColor: color,
           }}
           className='w-23 absolute left-[-9px] bottom-[-8px] bg-yellow flex border-[4px] justify-center items-center pt-1 rounded-[60px] text-darkBlue font-BPG-Nino-Mtavruli font-bold text-sm tracking-[1.2px]'
         >
