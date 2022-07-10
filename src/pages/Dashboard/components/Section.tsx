51bfab71ef71b94a7d5e85ee1c170726e4cb7cb4
Saba-Var <vartasashvili94@gmail.com>
2022-07-10 23:49:48 +0400

    chore: sort props of dashboard page components

--- src/pages/Dashboard/components/Section.tsx
+++ src/pages/Dashboard/components/Section.tsx
@@ -2,36 +2,38 @@ import { SectionProps } from 'pages/Dashboard/components/types'
 import { useNavigate } from 'react-router-dom'
 
 const Section: React.FC<SectionProps> = (props) => {
+  const { link, icon, title } = props
+
   const navigate = useNavigate()
   const currentURL = `/Dashboard/${props.link}`
   const isSelected = window.location.pathname.includes(props.link)
 
   const onClickHandler = () => {
-    if (props.link === 'Logout') {
+    if (link === 'Logout') {
       localStorage.removeItem('token')
 
       return navigate('/')
     }
 
-    if (props.link === 'Members') return navigate('/Dashboard/Members?page=1')
+    if (link === 'Members') return navigate('/Dashboard/Members?page=1')
     navigate(currentURL)
   }
 
   return (
     <div
-      data-cy={props.link}
+      data-cy={link}
       className={`flex transition-transform active:scale-95 cursor-pointer h-12 items-center gap-4 pl-7 ${
         isSelected && 'bg-contentWhite  '
       }`}
       onClick={onClickHandler}
     >
-      <div className='flex justify-center items-center w-7'>{props.icon}</div>
+      <div className='flex justify-center items-center w-7'>{icon}</div>
       <p
         className={`font-BPG-Nino-Mtavruli text-lg text-contentWhite pt-[5px] ${
           isSelected && '!text-darkBlue'
         }`}
       >
-        {props.title}
+        {title}
       </p>
     </div>
   )
