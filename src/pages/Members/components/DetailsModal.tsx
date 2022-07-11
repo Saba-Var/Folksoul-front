4fee83ca56cbe02a07680ef21d831321c6ab433b
Saba-Var <vartasashvili94@gmail.com>
2022-07-11 10:47:16 +0400

    style: make member details modal scrollable

--- src/pages/Members/components/DetailsModal.tsx
+++ src/pages/Members/components/DetailsModal.tsx
@@ -7,9 +7,7 @@ const DetailsModal: React.FC<DetailModal> = (props) => {
 
   return (
     <Modal title={`${name}~${instrument}`} setShowModal={props.setMemberModal}>
-      <div
-        className={`h-[500px] ${biography.length > 630 && 'overflow-y-scroll'}`}
-      >
+      <div className={`h-[500px] overflow-y-auto w-[105%] pr-[5%]`}>
         <div className='flex justify-center mb-4 mt-4 animate-slit-in-vertical'>
           <div
             className={`border bg-darkGray shadow-5xl border-white w-36 h-36 rounded-full flex justify-center items-center overflow-hidden`}
@@ -30,7 +28,12 @@ const DetailsModal: React.FC<DetailModal> = (props) => {
         <p className='text-sm tracking-wide text-center mb-3'>
           ორბიტალური დაშორება: <span className='font-bold'>{orbitLength}</span>
         </p>
-        <p className='text-justify'>{biography}</p>
+        <div
+          className='overflow-hidden animate-focus-in-expand whitespace-pre-line break-words pt-8 bg-transparent resize-none outline-none w-full h-max'
+          defaultValue={biography}
+        >
+          {biography}
+        </div>
       </div>
     </Modal>
   )
