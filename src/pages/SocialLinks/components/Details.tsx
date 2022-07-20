2dfdf4b301c87b1ae5c9fa4d622b5d34e3169eb8
Saba-Var <vartasashvili94@gmail.com>
2022-07-20 17:09:57 +0400

    chore: format link name and url if it is long

--- src/pages/SocialLinks/components/Details.tsx
+++ src/pages/SocialLinks/components/Details.tsx
@@ -3,14 +3,25 @@ import { DetailsProps } from 'pages/SocialLinks/components'
 const Details: React.FC<DetailsProps> = (props) => {
   const { linkName, url } = props
 
+  let socialLinkName = linkName
+  let socialLinkUrl = url
+
+  if (socialLinkName.length > 15) {
+    socialLinkName = `${socialLinkName.slice(0, 15)}...`
+  }
+
+  if (socialLinkUrl.length > 34) {
+    socialLinkUrl = `${socialLinkUrl.slice(0, 34)}...`
+  }
+
   return (
     <div className='flex gap-20 w-2/3'>
       <div className='w-32 text-center'>
         <p
-          data-cy={linkName}
+          data-cy={socialLinkName}
           className='font-BPG-Nino-Mtavruli text-contentWhite tracking-widest text-lg'
         >
-          {linkName}
+          {socialLinkName}
         </p>
       </div>
 
@@ -22,7 +33,7 @@ const Details: React.FC<DetailsProps> = (props) => {
           rel='noreferrer'
           target='_blank'
         >
-          {url}
+          {socialLinkUrl}
         </a>
       </div>
     </div>
